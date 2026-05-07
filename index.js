2da83e8deadf350b31bd7e949f90ed5f347ae88d
N7R-ON-TOP <N7Ry3r@gmail.com>
2026-05-07 14:10:21 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`✅ ${client.user.tag} شغال`);
+});
+
+client.on('messageCreate', message => {
+
+  if (message.author.bot) return;
+
+  if (message.content === "ق") {
+    message.reply("🔒 تم القفل");
+  }
+
+});
+
+client.login("حط_التوكن");
